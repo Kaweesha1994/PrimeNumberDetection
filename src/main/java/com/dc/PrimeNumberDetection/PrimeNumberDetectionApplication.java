fa263424e747ae8c45f147c693d66ad14b9e81d5
Kaweesha1994 <kaweeshahasarinda@gmail.com>
2022-04-22 17:46:46 +0530

    Initial commit

---
+++ src/main/java/com/dc/PrimeNumberDetection/PrimeNumberDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.dc.PrimeNumberDetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumberDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumberDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dc/PrimeNumberDetection/PrimeNumberDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.PrimeNumberDetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeNumberDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
