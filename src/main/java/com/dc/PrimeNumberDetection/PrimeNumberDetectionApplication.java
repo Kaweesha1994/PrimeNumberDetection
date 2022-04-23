dd356c44fb69cf6ced13885835f0b3b2abc8621e
Kaweesha1994 <kaweeshahasarinda@gmail.com>
2022-04-23 23:20:49 +0530

    register node to consul service registry

--- src/main/java/com/dc/PrimeNumberDetection/PrimeNumberDetectionApplication.java
+++ src/main/java/com/dc/PrimeNumberDetection/PrimeNumberDetectionApplication.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 import com.dc.PrimeNumberDetection.bully.Bully;
 
@@ -28,7 +29,32 @@ public class PrimeNumberDetectionApplication {
 
 		System.out.println("node id before : " + nodeId);
 
+		System.out.println("First arguement : " + args[0]);
+		System.out.println("Second arguement : " + args[1]);
+
 		Bully bully = new Bully(args[0], (int) (nodeId < 0 ? nodeId : nodeId * -1), Integer.parseInt(args[1]));
+
+		registerNode();
+
+	}
+
+	private static void registerNode() {
+
+		String url = "http://localhost:8500/v1/agent/service/register";
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		String data = "{\n" + "        \"Name\": \"" + Bully.getNodeName() + "\",\n" + "        \"ID\": \""
+				+ Bully.getNodeId() + "\",\n" + "        \"port\": " + Bully.getPort() + ",\n"
+				+ "        \"check\": {\n" + "            \"name\": \"Check Counter health " + Bully.getPort() + "\",\n"
+				+ "            \"tcp\": \"localhost:" + Bully.getPort() + "\",\n"
+				+ "            \"interval\": \"10s\",\n" + "            \"timeout\": \"1s\"\n" + "        }\n"
+				+ "    }";
+
+		System.out.println(data);
+
+		restTemplate.put(url, data);
+
 	}
 
 }
